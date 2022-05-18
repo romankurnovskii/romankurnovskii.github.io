96b0cffa30550dee39696365d5a24a3047544ba6
Roman Kurnovskii <r.kurnovskii@gmail.com>
2022-05-18 21:39:43 +0300

    css refactor
    Former-commit-id: b578b44d6ebfbd0e78da5982a0e4aa9c3ffb4378

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -26,12 +26,23 @@ module.exports = {
       }],
       'lg': '1.125rem',
       'xl': '1.25rem',
-      '2xl': '1.5rem',
+      '2xl': '1.5rem', // h1
       '3xl': '1.875rem',
-      '4xl': '2.25rem',
+      '4xl': '2.0rem',
       '5xl': '3rem',
       '6xl': '4rem',
       '7xl': '5rem',
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '650',
+      extrabold: '700',
+      black: '900',
     },
     extend: {
       height: {
